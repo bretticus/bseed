928eca6c6be6af79382255f1aa34577820b78765
Brett Millett <bretticus@gmail.com>
2016-04-29 23:20:45 -0600

    fixed file output as I was using file_exists but then using is_writeable which is never true if the file doesn't exist.

--- src/Commands/SeedCommand.php
+++ src/Commands/SeedCommand.php
@@ -5,6 +5,7 @@ namespace BambooSeeder\Commands;
 use BambooSeeder\Libraries\Engine;
 use BambooSeeder\Libraries\RendererFactory;
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Exception\RuntimeException;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
@@ -23,13 +24,9 @@ class SeedCommand extends Command {
 	protected function configure() {
 		$this->setName('seed')
 				->setDescription('Generate Seed Data')
-				->addArgument(
-						'number', InputArgument::OPTIONAL, 'How many seed records do you want to generate?', 10
-				)->addOption(
-				'filename', 'f', InputOption::VALUE_OPTIONAL, 'If set, the output will be written to filename instead of standard output.'
-		)->addOption(
-				'type', 't', InputOption::VALUE_OPTIONAL, 'For now, we only support the csv type', 'csv'
-		);
+				->addArgument('number', InputArgument::OPTIONAL, 'How many seed records do you want to generate?', 10)
+				->addOption('fileout', 'f', InputOption::VALUE_OPTIONAL, 'If set, the output will be written to file name specified instead of standard output.')
+				->addOption('type', 't', InputOption::VALUE_OPTIONAL, 'For now, we only support the csv type', 'csv');
 	}
 
 	/**
@@ -39,15 +36,18 @@ class SeedCommand extends Command {
 	 */
 	protected function execute(InputInterface $input, OutputInterface $output) {
 		$number = $input->getArgument('number');
-		$fileName = $input->getOption('filename');
+		$fileName = $input->getOption('fileout');
 		$type = $input->getOption('type');
 		$renderer = RendererFactory::create($type);
 		$records = Engine::render($number);
 		$renderer->setRecords($records);
 		$out = $renderer->render();
-		if (!file_exists($fileName) && is_writable($fileName)) {
+		if (null !== $fileName) {
+			if (file_exists($fileName)) {
+				throw new RuntimeException($fileName . ' already exists. Cowardly refusing to overwrite.');
+			}
 			file_put_contents($fileName, $out);
-			$output->writeln('<info>Wrote outout to ' . $fileName . '</info>');
+			$output->writeln('<info>Wrote output to ' . $fileName . '</info>');
 		} else {
 			$output->write($out);
 		}
